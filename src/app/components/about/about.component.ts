1a96df89ace48ef3ca31cb737096c7f96c782dcd
Cesar Rodrigo Nieto Lopez <cesar_rnl@hotmail.com>
2022-02-01 12:37:24 -0600

    se agrega componente about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import build from '@iconify/icons-mdi/office-building'
+import email from '@iconify/icons-mdi/email-multiple'
+import badge from '@iconify/icons-mdi/badge-account-horizontal'
+import birthday from '@iconify/icons-mdi/cake-variant'
+import github from '@iconify/icons-mdi/github'
+import linkedin from '@iconify/icons-mdi/linkedin-box'
 
 @Component({
   selector: 'app-about',
@@ -6,6 +12,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+
+  Build=build;
+  Email=email;
+  Badge=badge;
+  Birthday=birthday;
+  Github=github;
+  Linkedin=linkedin;
 
   constructor() { }
 
