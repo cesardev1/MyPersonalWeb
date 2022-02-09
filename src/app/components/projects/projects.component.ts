e9ea5db3549a64d0f4c2787d3f9de1587bee602f
Cesar Rodrigo Nieto Lopez <cesar_rnl@hotmail.com>
2022-02-09 12:25:29 -0600

    "Se agregan proyectos e imagenes de proyectos"

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -10,20 +10,26 @@ export class ProjectsComponent implements OnInit {
 
   ListaProjectos:Projecto[]=[
     {
-      Name:"Prueba",
-      Description:"Descripcion: Lorem ipsum dolor sit amet consectetur adipisicing Incidunt, aspernatur magni. Cum corrupti nisi totam ipsum natus provident, dolorum, inventore fugiat praesentium tempore, aut voluptate placeat libero atque! Reprehenderit, mollitia.",
+      Name:"Quiron",
+      Description:"Aplicacion para localizacion y contacto con medicos especialistas atravez del uso de mapas",
       tech:"XAMARIN",
-      link:"http://www.google.com.mx",
-      images:[{directorio:"../../../assets/Images/projectos/ejemplo/Status.jpg", titulo:"",text:""},
-              {directorio:"../../../assets/Images/projectos/ejemplo/Status1.jpg", titulo:"",text:""}]
+      link:"",
+      images:[{directorio:"../../../assets/Images/projectos/quironapp/quiron (1).jpg", titulo:"",text:""},
+              {directorio:"../../../assets/Images/projectos/quironapp/quiron (2).jpg", titulo:"",text:""},
+              {directorio:"../../../assets/Images/projectos/quironapp/quiron (3).jpg", titulo:"",text:""},
+              {directorio:"../../../assets/Images/projectos/quironapp/quiron (4).jpg", titulo:"",text:""},
+              {directorio:"../../../assets/Images/projectos/quironapp/quiron (5).jpg", titulo:"",text:""},
+             ]
     },
     {
-      Name:"Prueba2",
-      Description:"Descripcion: Lorem ipsum dolor sit amet consectetur adipisicing Incidunt, aspernatur magni. Cum corrupti nisi totam ipsum natus provident, dolorum, inventore fugiat praesentium tempore, aut voluptate placeat libero atque! Reprehenderit, mollitia.",
-      tech:"XAMARIN",
-      link:"http://www.google.com.mx",
-      images:[{directorio:"../../../assets/Images/projectos/ejemplo/Status3.jpg", titulo:"",text:""},
-              {directorio:"../../../assets/Images/projectos/ejemplo/Status4.jpg", titulo:"",text:""}]
+      Name:"CesarDev",
+      Description:"Pagina de perfil personal para uso como curriculm y portafolio asi como integracion de un blog personal",
+      tech:"Angular",
+      link:"https://github.com/cesardev1/MyPersonalWeb.git",
+      images:[{directorio:"../../../assets/Images/projectos/personalWeb/Screenshot_1.png", titulo:"",text:""},
+              {directorio:"../../../assets/Images/projectos/personalWeb/Screenshot_2.png", titulo:"",text:""},
+              {directorio:"../../../assets/Images/projectos/personalWeb/Screenshot_3.png", titulo:"",text:""},
+            ]
     }
   ]
   constructor() { }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import home from '@iconify/icons-mdi/memory'
+
 if (environment.production) {
   enableProdMode();
 }
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
