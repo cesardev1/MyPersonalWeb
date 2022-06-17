d4e5813ca402728a09a0c51a5b9d1a9b1cc91671
Cesar Rodrigo Nieto Lopez <cesar_rnl@hotmail.com>
2022-06-17 11:55:37 -0500

    change of api address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl:"https://api-personalces.herokuapp.com",
+  apiUrl:"https://www.theapi.info",//My API en produccion
+  //apiUrl:"https://localhost:7175", //test
   //apiUrl:"http://localhost:3000",
   urlContact: "/api/contact",
 };
